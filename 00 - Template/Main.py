9cdb5adf38437281fcfb3888033ad47096758b31
Roni Andarsyah <roni.andarsyah@gmail.com>
2022-02-04 21:29:45 +0700

    edit bytcode

--- 00 - Template/Main.py
+++ 00 - Template/Main.py
@@ -1,3 +1,7 @@
+import time
+start_time = time.time()
+
+
 print ("nanaonan")
 print ("nanaonan")
 print ("nanaonan")
@@ -11,4 +15,9 @@ a = 10 #ini komentar juga
 """ Ini adalah komentar menggunakan tanda kutif tiga dalam komentar multiline"""
 print(a)
 
+
+print(time.time() - start_time,"detik")
 #kita bisa compile python ke bytecode ()
+#apa itu bytcode
+#Cara compilenya , buke terminal dan ketikan dibawah ini
+#python -m py_compile Main.py
