2a505e2648a6275c449ef16f2678cade09bcb0f2
Roni Andarsyah <roni.andarsyah@gmail.com>
2022-02-04 22:28:11 +0700

    nambah file datafile.py

---
+++ 00 - Template/datafile.py
@@ -0,0 +1,17 @@
+# #Input data dari User
+
+#Data yang dimasukan harus STRING
+data = input("Masukan data: ")
+
+print ("data = ", data, ", type =", type(data))
+
+# #Jika kita ingin mengambil int, maka
+angka = float(input("masukan angka: "))
+angka = int(input("masukan angka: "))
+
+print ("data = ", angka, ", type =", type(angka))
+
+#Boolean aja
+biner = bool(int(input("masukan boolean: ")))
+
+print ("data = ", biner, ", type =", type(biner))
