dec6c903a7eb50220d3d99b3149d2193fcb86b72
Roni Andarsyah <roni.andarsyah@gmail.com>
2022-02-04 21:42:59 +0700

    edit bytcode

---
+++ 00 - Template/variable.py
@@ -0,0 +1,27 @@
+#Variable adalah tempat menympan data
+
+#Menyimpan nilai (asignment nilai)
+a = 10
+x = 5
+panjang = 100000
+
+
+print(a)
+print("Nilai a =", a)
+print("Nilai x =", x)
+print("Nilai panjang =", panjang)
+
+#Penamaan
+nilai_y = 15 #gunakan underscore
+print(nilai_y)
+nilaiZ = 17,5 #ini bisa
+print(nilaiZ)
+
+#pemanggilan kedua
+print ("Nilai a =", a)
+a = 2000
+print("Nilai a =", a)
+
+#assignment indirect
+b = a
+print ("Nilai b =", b)
